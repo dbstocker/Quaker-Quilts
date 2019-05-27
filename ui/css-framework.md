40d6c412182f39bb48bb764f8d34ba60515d3d3c
dbstocker <dstocker@notesettersinc.com>
2019-05-26 21:53:06 -0400

    playing with Bulma added a boilerplate nav

---
+++ ui/css-framework.md
@@ -0,0 +1,9 @@
+# Quaker Quilts CSS Framework Options
+
+## [Bulma](https://bulma.io/)
+
+## [Spectre](https://picturepan2.github.io/)
+
+## [GroundworkCSS](http://groundworkcss.github.io/groundwork/?url=docs/home)
+
+## [Cutestrap](https://www.cutestrap.com/)
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponentComponent
   ],
   imports: [
     BrowserModule
