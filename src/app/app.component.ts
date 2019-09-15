83fe7ea05aca42cec5ebd0f713b8471676ef765e
dbstocker <dstocker@notesettersinc.com>
2019-09-14 21:46:38 -0400

    ng-bootstrap installed and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quaker-quilts-ui';
+
+  model = {
+    left: true,
+    middle: false,
+    right: false
+  };
 }
